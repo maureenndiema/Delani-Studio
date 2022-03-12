c2aa26f3233c8f1c0546e0663979aabf4bc94410
maureenndiema <ndiemam@gmail.com>
2022-03-12 14:37:58 +0300

    add pending changes

--- JS/script.js
+++ JS/script.js
@@ -6,19 +6,19 @@
 // });
 
 $(document).ready(function(){
-// var contactForm= $("contactform")
-// $(function(){
-//     contactForm.on("submit", function(){
-//         e.preventDefault()
-//         let formValid = validContactForm();
-//         if (formValid){
-//             const name = ("input.name").value()
-//             alert("Thank you ${name}, your message has been received");
-//             clearForm();
-//         }
-//     }
-// );
-// });
+var contactForm= $("contactform")
+$(function(){
+    contactForm.on("submit", function(){
+        e.preventDefault()
+        let formValid = validContactForm();
+        if (formValid){
+            const name = ("input.name").value()
+            alert("Thank you ${name}, your message has been received");
+            clearForm();
+        }
+    }
+);
+});
 $("#design").click(function(){
     $(".design-hidden").toggle();
     $(".design-showing").toggle();
@@ -36,28 +36,28 @@ $("#product").click(function(){
 });
 });
 // validate contact form
-// function validateContactForm(){
-//     let isFormValid = true;
-//     const formInputs = contactForm.find("input,textarea")
+function validateContactForm(){
+    let isFormValid = true;
+    const formInputs = contactForm.find("input,textarea")
      
-//     formInputs.each(function(){
-//         if ($(this).value()){
-//             $(this).addClass(is-invalid);
-//             isFormValid = false;
-//         }
-//     }
-//     return isFormValid;
-// });
+    formInputs.each(function(){
+        if ($(this).value()){
+            $(this).addClass(is-invalid);
+            isFormValid = false;
+        }
+    }
+    return isFormValid;
+});
 
-// function removeFormErrors(){
-//     contactForm.find("input,textarea").on("keydown", function(){
-//         if ($(this).hasClass("is-invalid")){
-//             $(this).removeClass("is-invalid");
-//         }
-//     });
-// }
-// function clearForm(){
-//     contactForm.find("input,textarea").each(function){
-//         $(this).value("");
-//     }
-// }
+function removeFormErrors(){
+    contactForm.find("input,textarea").on("keydown", function(){
+        if ($(this).hasClass("is-invalid")){
+            $(this).removeClass("is-invalid");
+        }
+    });
+}
+function clearForm(){
+    contactForm.find("input,textarea").each(function){
+        $(this).value("");
+    }
+}

0a09289186f775a049f153f5f4d627cb39ed3780
maureenndiema <ndiemam@gmail.com>
2022-03-13 02:03:36 +0300

    add pending changes

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,19 @@
 $(document).ready(function () {
-    
     $("#design").click(function () {
         $(".design-hidden").fadeToggle();
         $(".design-showing").fadeToggle();
     });
-    
+
     $("#development").click(function () {
         $(".development-hidden").fadeToggle();
         $(".development-showing").fadeToggle();
     });
-    
+
     $("#product").click(function () {
         $(".product-hidden").fadeToggle();
         $(".product-showing").fadeToggle();
-
     });
- 
-    });
+});
 //     $(".overlay").hover(function(){
 //         $(this).animate({opacity:'1'});
 //     },
@@ -24,33 +21,25 @@ $(document).ready(function () {
 //         $(this).animate({opacity:'0'})
 //     })
 // });
-    // $("#work1 img").mouseover(function(){
-    //     $("#work1 img").hide();
-    //     $("#overlay1").show();
-    // });
-    // $("#work1 img").mouseout(function(){
-    //     $("#work1 img").show();
-    //     $("#overlay1").hide();
-    // });
-    
-    
- 
+// $("#work1 img").mouseover(function(){
+//     $("#work1 img").hide();
+//     $("#overlay1").show();
+// });
+// $("#work1 img").mouseout(function(){
+//     $("#work1 img").show();
+//     $("#overlay1").hide();
+// });
 
-
-// validate contact form
 function validate() {
-    
-        var name = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
-        var message = document.getElementById("message").value;
-        if (name == "" || email  == ""|| message  ==""){
-          alert("Please enter your name, email and message!");
-           return false;
-    
-        }
-        else{
-          alert (name + ",hello,thank you for contacting us. We will get back to you as soon as possible.");
-              }
-        };
 
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (name == "" || email == "" || message == "") {
+        alert("Please enter your name, email and message!");
+        return false;
 
+    } else {
+        alert(name + ",hello,thank you for contacting us. We will get back to you as soon as possible.");
+    }
+};

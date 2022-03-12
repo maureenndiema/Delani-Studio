e66c9ada2ec72b8ce0d4459141f906242198fa7c
maureenndiema <ndiemam@gmail.com>
2022-03-12 12:20:25 +0300

    commit pending changes

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,7 @@
 //     $("hidden").show()
 //     $()
 // });
+$(document).ready(function(){
 var contactForm= $("contactform")
 $(function(){
     contactForm.on("submit", function(){
@@ -16,3 +17,18 @@ $(function(){
         }
     }
 });
+$("#design").click(function(){
+    $(".design-hidden").toggle();
+    $(".design-showing").toggle();
+});
+
+$("#development").click(function(){
+    $(".development-hidden").toggle();
+    $(".development-showing").toggle();
+});
+
+$("#product-management").click(function(){
+    $(".product-management-hidden").toggle();
+    $(".product-management-showing").toggle();
+
+}):

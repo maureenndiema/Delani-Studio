bf1acc58d31830194a378ee44331a8486fe3ae5b
maureenndiema <ndiemam@gmail.com>
2022-03-13 11:24:37 +0300

    add pending changes

--- JS/script.js
+++ JS/script.js
@@ -14,21 +14,49 @@ $(document).ready(function () {
         $(".product-showing").fadeToggle();
     });
 });
-//     $(".overlay").hover(function(){
-//         $(this).animate({opacity:'1'});
-//     },
-//     function(){
-//         $(this).animate({opacity:'0'})
-//     })
-// });
-// $("#work1 img").mouseover(function(){
-//     $("#work1 img").hide();
-//     $("#overlay1").show();
-// });
-// $("#work1 img").mouseout(function(){
-//     $("#work1 img").show();
-//     $("#overlay1").hide();
-// });
+
+$(document).ready(function(){
+    $("#work1").mouseover(function(){
+        $("#overlay1").show();
+    }).mouseout(function(){
+        $("#overlay1").hide();
+    });
+    $("#work2").mouseover(function(){
+        $("#overlay2").show();
+    }).mouseout(function(){
+        $("#overlay2").hide();
+    });
+    $("#work3").mouseover(function(){
+        $("#overlay3").show();
+    }).mouseout(function(){
+        $("#overlay3").hide();
+    });
+    $("#work4").mouseover(function(){
+        $("#overlay4").show();
+    }).mouseout(function(){
+        $("#overlay4").hide();
+    });
+    $("#work5").mouseover(function(){
+        $("#overlay5").show();
+    }).mouseout(function(){
+        $("#overlay5").hide();
+    });
+    $("#work6").mouseover(function(){
+        $("#overlay6").show();
+    }).mouseout(function(){
+        $("#overlay6").hide();
+    });
+    $("#work7").mouseover(function(){
+        $("#overlay7").show();
+    }).mouseout(function(){
+        $("#overlay7").hide();
+    });
+    $("#work8").mouseover(function(){
+        $("#overlay8").show();
+    }).mouseout(function(){
+        $("#overlay8").hide();
+    });
+})
 
 function validate() {
 
